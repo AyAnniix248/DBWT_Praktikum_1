cd9b801c0764dd456e38e44cee8f243bea3c5ee9
Constantin Kalversberg <constantin.kalversberg@alumni.fh-aachen.de>
2019-10-18 15:11:42 +0200

    header footer excluded

---
+++ untitled/html/Start.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <script src="../js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                crossorigin="anonymous"></script>
+        <title>Meilenstein 1</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <!--container-fluid ermöglicht die maximale breite eines containers -->
+        <!-- Navigation -->
+        <?php include('Header.html');?>
+
+        <!-- schöner machen -->
+        <img src="../img/top(probe).jpg" alt="Kantine" class="mx-auto d-block myPic" width="100%" height="20%">
+        <!-- Main Content -->
+        <!-- column, row Prinzip. Max. col.: 12 | lg für large screen-->
+        <div class="container-fluid main-content">
+            <div class="row my-row">
+                <div class="col-lg-3 my-col">
+                    <p class="myText">Der Dienst e-Mensa ist noch beta. Sie können bereits <a href="#">Mahlzeiten</a>
+                        durchstöbern, aber noch nicht bestellen.</p>
+                </div>
+                <div class="col-lg-7 my-col">
+                    <h1>Leckere Gerichte vorbestellen</h1>
+                    <p class="myText">... und gemeinsam mit Kommilitonen und Freunden essen, aber leider ist das bier alle
+                        :((</p>
+                </div>
+                <div class="col-lg-2 my-col">
+                    <button type="button" class="btn btn-outline-secondary">Registrieren</button>
+                    <br/>
+                    <button type="button" class="btn btn-primary">Anmelden</button>
+                </div>
+            </div>
+            <div class="row my-row">
+                <div class="col-lg-3 my-col">
+                    <p class="myText"> Registrieren Sie sich <a href="#">hier</a>, um über die Veröffentlichung des Dienstes per
+                        Mail informiert zu werden.</p>
+                </div>
+                <div class="col-lg-9 my-col">
+                    <img src="../img/top(probe).jpg" alt="..." class="mx-auto d-block" width="100%">
+                </div>
+            </div>
+        </div>
+        <?php include('Footer.html');?>
+    </body>
+</html>
+
+<!-- Img Slider
+<div id="demo" class="carousel slide" data-ride="carousel">
+     Indicators
+    <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+ The slideshow
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../img/1.jpg">
+        </div>
+        <div class="carousel-item">
+            <img src=../img/2.jpg">
+        </div>
+        <div class="carousel-item">
+            <img src="../img/3.jpg">
+        </div>
+    </div>
+ Left and right controls
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </a>
+</div>
+-->
