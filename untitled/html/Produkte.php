b89806c04f50ece8e96a0edffe8c234f970bba8d
Constantin Kalversberg <constantin.kalversberg@alumni.fh-aachen.de>
2019-10-18 15:16:17 +0200

    link fix

--- untitled/html/Produkte.php
+++ untitled/html/Produkte.php
@@ -1,18 +1,17 @@
 <html lang="de">
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <script src="../js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    <title>Meilenstein 1</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-</head>
-<body>
-<!--container-fluid ermöglicht die maximale breite eines containers -->
-<!-- Navigation -->
-<?php include('Header.html');?>
-<?php include('Footer.html');?>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <script src="../js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                crossorigin="anonymous"></script>
+        <title>Meilenstein 1</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <?php include('Header.html');?>
+
+        <?php include('Footer.html');?>
+    </body>
 </html>
 
