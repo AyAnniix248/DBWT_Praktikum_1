2ed063bf051d71af6ea1d7b46d02734314df5b32
AyAnniix248 <yannik.niewoehner@gmail.com>
2019-10-19 18:01:09 +0200

    tab geändert

--- untitled/html/Detail.php
+++ untitled/html/Detail.php
@@ -10,19 +10,27 @@
 </head>
 <body>
 <?php include('Header.html'); ?>
+<script>
+    $(document).ready(function(){
+        $(".nav-tabs a").click(function(){
+            $(this).tab('show');
+        });
+    });
+</script>
 <div class="row">
     <!--Login-->
     <div class="col-lg-3 border-dark p-3">
         <form class="form-signin">
             <h2 class="form-signin-heading">Login</h2>
-            <input type="text" class="form-control" name="user" placeholder="Benutzer" required="" autofocus="" />
+            <input type="text" class="form-control" name="user" placeholder="Benutzer" required="" autofocus=""/>
             <input type="password" class="form-control" name="password" placeholder="Passwort" required=""/>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Anmelden</button>
         </form>
     </div>
     <div class="col-lg-7">
         <h1>Details für "Falafel"</h1>
-        <img src="../img/Fh-aachen.png" alt="..." class="rndImg" width="100%"><p></p>
+        <img src="../img/Fh-aachen.png" alt="..." class="rndImg" width="100%">
+        <p></p>
     </div>
     <div class="col-lg-2 preis">
         <p>Gast-Preis</p>
@@ -32,31 +40,40 @@
 </div>
 <div class="row">
     <div class="col-lg-3">
-        <p class="smallerFont">Melden Sie sich jetzt an, um die wirklich viel günstigeren Preise für Mitarbeiter oder Studenten zu sehen</p>
+        <p class="smallerFont">Melden Sie sich jetzt an, um die wirklich viel günstigeren Preise für Mitarbeiter oder
+            Studenten zu sehen</p>
     </div>
     <!-- Tab system-->
     <div class="col-lg-6">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a class="nav-link active" id="beschreibung-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
-                   aria-selected="true">Beschreibungen</a>
+                <a class="nav-link active" href="#beschreibungen">Beschreibungen</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="zutaten-tab" data-toggle="tab" href="#zutaten" role="tab" aria-controls="profile"
-                   aria-selected="false">Zutaten</a>
+                <a class="nav-link" href="#zutaten">Zutaten</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="bewerungen-tab" data-toggle="tab" href="#bewertungen" role="tab" aria-controls="contact"
-                   aria-selected="false">Bewertungen</a>
+                <a class="nav-link" href="#bewertung">Bewertung</a>
             </li>
         </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Teigtasche mit Falafel aus Kichererbsen und Sesam, dazu passt hervorragend der Krautsalat.</div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Nix</div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Nix</div>
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div id="beschreibungen" class="container tab-pane fade">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.</p>
+            </div>
+            <div id="zutaten" class="container tab-pane fade"><br>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.</p>
+            </div>
+            <div id="bewertung" class="container tab-pane fade"><br>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                    totam rem aperiam.</p>
+            </div>
         </div>
     </div>
 </div>
+
 <?php include('Footer.html'); ?>
 </body>
 </html>
